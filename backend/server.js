a72403570131df6d54a14664ec4c726a3db6eff7
Nishit Mistry <nishitmistry@Nishits-MacBook-Air-2.local>
2025-11-25 11:22:26 -0600

    Ensure backend routes/controllers/models are committed

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use("/api/events", eventRoutes);
 
 
 // 7️⃣ Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
